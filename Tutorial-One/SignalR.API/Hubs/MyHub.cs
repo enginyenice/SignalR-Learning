435a2ae06c589d593219bfdbe3caca6f39762321
Engin Yenice <enginyenice2626@gmail.com>
2021-06-20 18:47:14 +0300

    SignalR Server Hub Clients.Group-2

--- Tutorial-One/SignalR.API/Hubs/MyHub.cs
+++ Tutorial-One/SignalR.API/Hubs/MyHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SignalR.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,12 +18,20 @@ namespace SignalR.API.Hubs
         private static List<String> Names { get; set; } = new List<string>();
         private static int ClientCount { get; set; } = 0;
         public static int TeamCount { get; set; } = 7;
+        private readonly AppDbContext _context;
+
+        public MyHub(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public async Task SetName(string name)
         {
-            if(Names.Count >= MyHub.TeamCount)
+            if (Names.Count >= MyHub.TeamCount)
             {
-                await Clients.Caller.SendAsync(method:"Error",$"Takım en fazla {MyHub.TeamCount} kişi olabilir");
-            } else
+                await Clients.Caller.SendAsync(method: "Error", $"Takım en fazla {MyHub.TeamCount} kişi olabilir");
+            }
+            else
             {
                 MyHub.Names.Add(name);
                 await Clients.All.SendAsync(method: "ReceiveName", arg1: name);
@@ -36,7 +46,7 @@ namespace SignalR.API.Hubs
         public async override Task OnConnectedAsync()
         {
             ClientCount++;
-            await Clients.All.SendAsync(method:"ReceiveClientCount",ClientCount);
+            await Clients.All.SendAsync(method: "ReceiveClientCount", ClientCount);
             await base.OnConnectedAsync();
         }
         public async override Task OnDisconnectedAsync(Exception exception)
@@ -44,6 +54,44 @@ namespace SignalR.API.Hubs
             ClientCount--;
             await Clients.All.SendAsync(method: "ReceiveClientCount", ClientCount);
             await base.OnDisconnectedAsync(exception);
+        }
+
+        //Groups
+        public async Task AddToGroup(string teamName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, teamName);
+
+        }
+        public async Task RemoveToGroup(string teamName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, teamName);
+        }
+        public async Task SendNameByGroup(string name, string teamName)
+        {
+            var team = _context.Teams.Where(p => p.Name == teamName).FirstOrDefault();
+            if (team != null)
+            {
+                team.Users.Add(new User { Name = name });
+            }
+            else
+            {
+                var newTeam = new Team { Name = teamName };
+                _context.Teams.Add(newTeam);
+                newTeam.Users.Add(new User { Name = name });
+            }
+
+            await _context.SaveChangesAsync();
+            await Clients.Group(teamName).SendAsync("ReceiveMessageByGroup",name,teamName);
+
+        }
+        public async Task GetNamesByGroup()
+        {
+            var teams = _context.Teams.Include(p => p.Users).Select(p => new
+            {
+                teamName = p.Name,
+                users = p.Users.ToList()
+            });
+            await Clients.All.SendAsync("ReceiveNamesByGroup",teams);
         }
     }
 }
