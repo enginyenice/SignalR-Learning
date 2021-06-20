b2b40d2d06b0a5bb237bd7aaa881d5a585d4100d
Engin Yenice <enginyenice2626@gmail.com>
2021-06-20 16:23:50 +0300

    Add project files.

---
+++ Tutorial-One/SignalR.API/Hubs/MyHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Hubs
+{
+    public class MyHub : Hub
+    {
+        public static List<String> Names { get; set; } = new List<string>();
+        public async Task SetName(string name)
+        {
+            MyHub.Names.Add(name);
+            await Clients.All.SendAsync(method:"ReceiveName",arg1:name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync(method: "ReceiveNames", arg1: Names);
+        }
+    }
+}
