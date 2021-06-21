b037be5c50847a225591c905c83d46392b32bf93
Engin Yenice <enginyenice2626@gmail.com>
2021-06-21 10:24:11 +0300

    CovidService

---
+++ Tutorial-Two/SignalR.Covid19Chart.API/Models/CovidService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.Covid19Chart.API.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Covid19Chart.API.Models
+{
+    public class CovidService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<CovidHub> _hubContext;
+        public CovidService(AppDbContext context, IHubContext<CovidHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Covid> GetList()
+        {
+            return _context.Covids.AsQueryable();
+
+        }
+
+        public async Task SaveCovid(Covid covid)
+        {
+            await _context.Covids.AddAsync(covid);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveCovidList","Gidecek data....");
+        }
+
+
+
+    }
+}
--- Tutorial-Two/SignalR.Covid19Chart.API/Startup.cs
+++ Tutorial-Two/SignalR.Covid19Chart.API/Startup.cs
@@ -28,6 +28,7 @@ namespace SignalR.Covid19Chart.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<CovidService>();
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
