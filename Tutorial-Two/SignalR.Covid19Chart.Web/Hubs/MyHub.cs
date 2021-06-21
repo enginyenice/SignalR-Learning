b9e28ea560fac1c6758f6c5f8d7a73268df6e675
Engin Yenice <enginyenice2626@gmail.com>
2021-06-21 12:14:31 +0300

    Covid19 Chart Uygulaması-6(SignalR Client) - WEB Tarafı

---
+++ Tutorial-Two/SignalR.Covid19Chart.Web/Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Covid19Chart.Web.Hubs
+{
+    public class MyHub : Hub
+    {
+    }
+}
--- Tutorial-Two/SignalR.Covid19Chart.Web/Startup.cs
+++ Tutorial-Two/SignalR.Covid19Chart.Web/Startup.cs
@@ -1,20 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using SignalR.Covid19Chart.API.Hubs;
-using SignalR.Covid19Chart.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SignalR.Covid19Chart.API
+namespace SignalR.Covid19Chart.Web
 {
     public class Startup
     {
@@ -28,21 +23,10 @@ namespace SignalR.Covid19Chart.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy", builder =>
-                {
-                    builder.WithOrigins("https://localhost:44390").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-                });
-            });
 
-            services.AddScoped<CovidService>();
-            services.AddDbContext<AppDbContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
-            });
-            services.AddSignalR();
-            services.AddControllers();
+
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,19 +36,25 @@ namespace SignalR.Covid19Chart.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                
-                endpoints.MapControllers();
-                endpoints.MapHub<CovidHub>("/CovidHub");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
